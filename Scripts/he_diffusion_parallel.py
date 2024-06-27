fda2d7dbb79c245b205acc5a27a802b713530482
Samanyu-T <samanyut8@gmail.com>
2024-06-27 15:47:30 +0100

    update

--- Scripts/he_diffusion_parallel.py
+++ Scripts/he_diffusion_parallel.py
@@ -23,54 +23,59 @@ n_replica = n_procs // n_temp
 
 temp_id = proc_id // n_temp
 
-replica_id = proc_id % n_replica
-
-temp = temp_arr[temp_id]
-
-init_dict = {}
-
-with open('init_param.json', 'r') as file:
-    init_dict = json.load(file)
-
 output_folder = 'He_Diffusion'
 
-save_folder = os.path.join(output_folder,'Temp_%d' % temp , 'Iteration_%d' % replica_id)
-if not os.path.exists(output_folder):
-    os.mkdir(output_folder)
-    os.mkdir(save_folder)
+if proc_id == 0:
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+comm.barrier()
 
-init_dict['orientx'] = [1, 0, 0]
+for i in range(10):
+    replica_id = proc_id % n_replica + i * n_replica
 
-init_dict['orienty'] = [0, 1, 0]
+    temp = temp_arr[temp_id]
 
-init_dict['orientz'] = [0, 0, 1]
+    init_dict = {}
 
-init_dict['size'] = 7
+    with open('init_param.json', 'r') as file:
+        init_dict = json.load(file)
 
-init_dict['surface'] = 0
+    save_folder = os.path.join(output_folder,'Temp_%d' % temp , 'Iteration_%d' % replica_id)
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
 
-init_dict['output_folder'] = output_folder
+    init_dict['orientx'] = [1, 0, 0]
 
-lmp_class = LammpsParentClass(init_dict, comm, proc_id)
+    init_dict['orienty'] = [0, 1, 0]
 
-lmp = lammps(cmdargs=['-screen', 'none', '-echo', 'none', '-log', 'none'])
+    init_dict['orientz'] = [0, 0, 1]
 
-lmp.commands_list(lmp_class.init_from_box()) 
+    init_dict['size'] = 7
 
-_xyz = init_dict['size'] // 2 + np.array([0.25, 0.5, 0])
+    init_dict['surface'] = 0
 
-lmp.command('create_atoms 3 single %f %f %f' % (_xyz[0], _xyz[1], _xyz[2]))
+    init_dict['output_folder'] = output_folder
 
-rng = np.random.randint(low = 1, high = 100000)
+    lmp_class = LammpsParentClass(init_dict, comm, proc_id)
 
-lmp.command('group mobile type 2 3')
+    lmp = lammps(comm=comm_split,cmdargs=['-screen', 'none', '-echo', 'none', '-log', 'none'])
 
-lmp.command('velocity all create %f %d rot no dist gaussian' % (2*temp, rng))
+    lmp.commands_list(lmp_class.init_from_box()) 
 
-lmp.command('fix fix_temp all nve')
+    _xyz = init_dict['size'] // 2 + np.array([0.25, 0.5, 0])
 
-lmp.command('dump mydump mobile custom 1000 %s/out.*.atom id type x y z'  % save_folder)
+    lmp.command('create_atoms 3 single %f %f %f' % (_xyz[0], _xyz[1], _xyz[2]))
 
-lmp.command('timestep 1e-3')
+    rng = np.random.randint(low = 1, high = 100000)
 
-lmp.command('run %d' % (5e6))
+    lmp.command('group mobile type 2 3')
+
+    lmp.command('velocity all create %f %d rot no dist gaussian' % (2*temp, rng))
+
+    lmp.command('fix fix_temp all nve')
+
+    lmp.command('dump mydump mobile custom 1000 %s/out.*.atom id type x y z'  % save_folder)
+
+    lmp.command('timestep 1e-3')
+
+    lmp.command('run %d' % (5e6))
