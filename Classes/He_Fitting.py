1a8e67a0ca40ba7d5f1c9c3acf7010f7ec3d1a7d
Samanyu-T <samanyut8@gmail.com>
2024-08-17 22:13:47 +0100

    up

--- Classes/He_Fitting.py
+++ Classes/He_Fitting.py
@@ -1497,53 +1497,53 @@ def genetic_alg(n_knots, comm, proc_id, work_dir = '../Optim_Local', save_folder
 
             )       
 
-    loss = np.empty((0))
-    samples = np.empty((0, 14))
+    # loss = np.empty((0))
+    # samples = np.empty((0, 14))
 
 
-    for i in range(112):
-        _loss = np.loadtxt('Fitting_Output_FS_Final/Genetic_Algorithm/Loss_%d.txt' % i)
-        _samples = np.loadtxt('Fitting_Output_FS_Final/Genetic_Algorithm/Samples_%d.txt' % i)
+    # for i in range(112):
+    #     _loss = np.loadtxt('Fitting_Output_FS_Final/Genetic_Algorithm/Loss_%d.txt' % i)
+    #     _samples = np.loadtxt('Fitting_Output_FS_Final/Genetic_Algorithm/Samples_%d.txt' % i)
 
-        loss = np.hstack([loss, _loss])
-        samples = np.vstack([samples, _samples])
+    #     loss = np.hstack([loss, _loss])
+    #     samples = np.vstack([samples, _samples])
 
-    idx = np.argsort(loss)
-    loss = loss[idx]
-    samples = samples[idx]
+    # idx = np.argsort(loss)
+    # loss = loss[idx]
+    # samples = samples[idx]
 
-    if proc_id == 0:
+    # if proc_id == 0:
         
-        x0 =  np.array([
-                    1.7015e+00,  4.2490e-01, 
-                    0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0,
-                    # 0, 0, 0, 0, 0, 0,
-                    # 0, 0, 0, 0, 0, 0,
-                    -1.1982e+00,  3.1443e+00, -3.2970e-01, -2.2820e-01,  4.1590e-01, -4.7750e-01 ,
-                    6.8130e-01, -3.8090e-01,  6.3500e-02,  8.6000e-03,  -9.4000e-03, 1.3100e-02
-                    ])
-    else:
+    #     x0 =  np.array([
+    #                 1.7015e+00,  4.2490e-01, 
+    #                 0, 0, 0, 0, 0, 0,
+    #                 0, 0, 0, 0, 0, 0,
+    #                 # 0, 0, 0, 0, 0, 0,
+    #                 # 0, 0, 0, 0, 0, 0,
+    #                 -1.1982e+00,  3.1443e+00, -3.2970e-01, -2.2820e-01,  4.1590e-01, -4.7750e-01 ,
+    #                 6.8130e-01, -3.8090e-01,  6.3500e-02,  8.6000e-03,  -9.4000e-03, 1.3100e-02
+    #                 ])
+    # else:
 
-        x_trial = samples[np.random.randint(low = 1, high = 1000)]
+    #     x_trial = samples[np.random.randint(low = 1, high = 1000)]
 
-        x0 = np.hstack([ x_trial[:2],
-                        0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0,
-                        # 0, 0, 0, 0, 0, 0,
-                        # 0, 0, 0, 0, 0, 0,
-                        x_trial[2:]
-                        ])
+    #     x0 = np.hstack([ x_trial[:2],
+    #                     0, 0, 0, 0, 0, 0,
+    #                     0, 0, 0, 0, 0, 0,
+    #                     # 0, 0, 0, 0, 0, 0,
+    #                     # 0, 0, 0, 0, 0, 0,
+    #                     x_trial[2:]
+    #                     ])
         
-        bounds_arr = np.array(bounds)
+    #     bounds_arr = np.array(bounds)
 
-        for i, _x in enumerate(x0):
-            x0[i] = np.clip(_x, a_min=bounds_arr[i,0], a_max=bounds_arr[i, 1])
+    #     for i, _x in enumerate(x0):
+    #         x0[i] = np.clip(_x, a_min=bounds_arr[i,0], a_max=bounds_arr[i, 1])
 
     print('x_init')
     sys.stdout.flush()
     differential_evolution(loss_func, bounds, args=(data_ref, fitting_class, False, True, save_folder),
-                                    init='latinhypercube', mutation=1.5, recombination=0.25, popsize=50, maxiter=50, polish=True, x0=x0)
+                                    init='latinhypercube', mutation=1.5, recombination=0.25, popsize=50, maxiter=50, polish=True)
 
     # res = differential_evolution(loss_func, bounds = bounds, args=(data_ref, fitting_class, diag, write, save_folder), popsize=50)
 
