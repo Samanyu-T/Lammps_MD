e223d3fd40ea96b7682a6a61a95745f448c0fd15
Samanyu-T <samanyut8@gmail.com>
2024-08-21 21:30:13 +0100

    update

--- Classes/He_Fitting.py
+++ Classes/He_Fitting.py
@@ -1437,7 +1437,7 @@ def gaussian_sampling(n_knots, comm, proc_id, mean, cov, max_time=3, work_dir = 
 
         loss = loss_func(sample, data_ref, fitting_class)
         
-        print(sample, loss)
+        # print(sample, loss)    
         
         idx += 1
 
