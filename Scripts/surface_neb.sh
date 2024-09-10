e85c0b588ad9afe7af3cd16293812f4ce31c9e95
Samanyu-T <samanyut8@gmail.com>
2024-09-10 12:06:18 +0100

    Update surface_neb.sh

--- Scripts/surface_neb.sh
+++ Scripts/surface_neb.sh
@@ -4,13 +4,13 @@ num_procs=$1  # Assign the first argument to the number of processors
 lmp_exec=/home/ir-tiru1/lammps/src/lmp_intel_cpu_intelmpi
 
 # Run the first NEB simulation
-mpiexec -np $num_procs $lmp_exec -p ${num_procs}x1 -in orient_100.neb
+mpiexec -np $num_procs $lmp_exec -p ${num_procs}x1 -in orient100.neb
 python read_neb_log.py orient_100.txt
 
 # Run the second NEB simulation
-mpiexec -np $num_procs $lmp_exec -p ${num_procs}x1 -in orient_110.neb
+mpiexec -np $num_procs $lmp_exec -p ${num_procs}x1 -in orient110.neb
 python read_neb_log.py orient_110.txt
 
 # Run the third NEB simulation
-mpiexec -np $num_procs $lmp_exec -p ${num_procs}x1 -in orient_111.neb
+mpiexec -np $num_procs $lmp_exec -p ${num_procs}x1 -in orient111.neb
 python read_neb_log.py orient_111.txt
